cbb7ad0bd7cc9f114f61ad87c7e027934d6a4727
Boris Paschenko <i_hope-you_die@mail.ru>
2017-08-26 20:47:10 +0300

    first name scaper

--- main/scrapper.py
+++ main/scrapper.py
@@ -4,9 +4,10 @@ class Parser:
     def __init__(self):
         self.driver = webdriver.Firefox()
 
-    def get_name(self, keywords):
+    def get_name(self, keywords=None):
         if keywords:
-            pass
+            name = ''
         else:
-            pass
+            self.driver.get('https://www.behindthename.com/random/random.php?number=1&gender=both&surname=&all=yes')
+            name = self.driver.find_element_by_class_name('plain').text
         return name
