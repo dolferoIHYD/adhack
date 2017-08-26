99eb8bb5930d1f1a89620e06f9c5805f38ef3bcc
Captain <whereismybas@gmail.com>
2017-08-27 03:37:44 +0400

    things

--- main/scrapper.py
+++ main/scrapper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import time
 
 class Parser:
     def __init__(self):
@@ -21,3 +22,17 @@ class Parser:
         while(slogan==''):
             slogan = self.driver.find_element_by_id('slog').text
         return slogan
+
+
+    def get_logo(self, name, keyword):
+        self.driver.get('https://www.logoshuffle.com/Logo#SelectedIcons=&Name={}&Slogan=&Keywords={}&IgnoreIcons=&SelectedIcons=&allSelectedText=Alle+ausgew%C3%A4hlt&fontFamilies%5B%5D=Serif&fontFamilies%5B%5D=SansSerif&fontFamilies%5B%5D=Handwriting&fontFamilies%5B%5D=Other&Color1=%23000010&Color2=%23757575&Color3=%23ABABAB&ColorBg=%23FFFFFF&ColorSchemeParams=1&ResetConstraints=True'.format(name, keyword))
+        #while(image==0):
+        try:
+            time.sleep(15)
+            image  = self.driver.find_element_by_id('ls-logo_0')
+        except Exception as e:
+            image = 0
+            print(e)
+        link = image.get_attribute('src')
+       # return 'https://www.logoshuffle.com()'.format(link)
+        return link
