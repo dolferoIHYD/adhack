e0ade645be910ca864b0d1c6b556190bc6795ec0
Boris Paschenko <i_hope-you_die@mail.ru>
2017-08-27 15:45:54 +0300

    shit

--- main/views.py
+++ main/views.py
@@ -21,9 +21,34 @@ def landing_view(request, slug):
         name = p.get_name()
     ctx = {
         'name': name,
-        #'object': p,
+        'object': p,
         'logo': p.get_logo(name, keyword),
         'slogan': p.get_slogan(name),
-
+        'pictures': p.get_pictures(keyword),
+        'card': p.create_card(name)
     }
+    p.driver.quit()
+    return render(request, 'landing.html', ctx)
+
+
+def preview(request, slug):
+    name = request.GET.get('name')
+    keyword = request.GET.get('keyword')
+
+    p = Parser()
+
+    if not check_domain(name):
+        return render(request, 'error.html')
+
+    if not name:
+        name = p.get_name()
+    ctx = {
+        'name': name,
+        'object': p,
+        'logo': p.get_logo(name, keyword),
+        'slogan': p.get_slogan(name),
+        'pictures': p.get_pictures(keyword),
+        'card': p.create_card(name)
+    }
+    p.driver.quit()
     return render(request, 'site2.html', ctx)
