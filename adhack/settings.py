92144a62ad2d4a66846ba2df0a72135d633e7a14
Boris Paschenko <i_hope-you_die@mail.ru>
2017-08-27 03:01:19 +0300

    restructured

--- adhack/settings.py
+++ adhack/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'adhack.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
