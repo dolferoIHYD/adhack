56607ec707552ccaefb726397b7227792813e322
Boris Paschenko <i_hope-you_die@mail.ru>
2017-08-27 15:59:25 +0300

    shit

--- main/urls.py
+++ main/urls.py
@@ -5,7 +5,7 @@ from main import views
 urlpatterns = [
     url(r'^$', views.index),
     url(r'^(?P<slug>[-a-z-A-Z-а-я-А-Я-0-9-_+.]+)/$', views.landing_view),
-    #url(r'^(?P<slug>[-a-z-A-Z-а-я-А-Я-0-9-_+.]+)/preview/$', views.preview),
+    url(r'^(?P<slug>[-a-z-A-Z-а-я-А-Я-0-9-_+.]+)/preview/$', views.preview),
 
 
 ]
